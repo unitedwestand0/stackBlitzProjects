a46e6f2041097bf3b778969d40db481ade234fb4
unitedwestand0 <46530671+unitedwestand0@users.noreply.github.com>
2023-07-09 20:16:40 -0500

    2 files modified

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const joke = document.querySelector('#joke');
+const refreshBtn = document.querySelector('#refreshBtn');
 const uri = 'https://icanhazdadjoke.com/';
 
 let xhr = new XMLHttpRequest();
 xhr.addEventListener('load', function() {
-  app.innerHTML = `<h1>${this.responseText}</h1>`;
+  joke.innerHTML = `<h1>${this.responseText}</h1>`;
 });
 
-xhr.open('GET', uri);
-xhr.setRequestHeader('Accept', 'text/plain');
-xhr.send();
+refreshBtn.addEventListener('click', function(){
+  xhr.open('GET', uri);
+  xhr.setRequestHeader('Accept', 'text/plain');
+  xhr.send();
+});
